3fb660c5c95d2d4cdd3012fb927126f142ece136
Michael Cao <michael.cao88@gmail.com>
2013-03-08 23:47:35 -0800

    add customizations

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by_email(params[:email].downcase)
+    if user && user.authenticate(params[:password])
       sign_in user
+      flash[:success] = "Welcome back #{user.name}!"
       redirect_to user
     else
       flash.now[:error] = 'Invalid email/password combination'
